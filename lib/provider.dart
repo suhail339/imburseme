168fda3d1fbe7214799acb001481530109b2f9da
usman umar <usmanumar90@gmail.com>
2019-06-01 18:06:17 +0500

    provider update

--- lib/provider.dart
+++ lib/provider.dart
@@ -9,22 +9,4 @@ class FirebaseProvider {
     invocieMap['invoiceDetail'] = invoice;
     Firestore.instance.collection('Invoices').add(invocieMap);
   }
-
-  // static removePlayer(String id) {
-  //   Firestore.instance.runTransaction((Transaction transaction) async {
-  //     await reference.document(id).delete().catchError((error) {
-  //       print(error);
-  //     });
-  //   });
-  // }
-
-  // static updatePlayer(String id, String newName) {
-  //   Firestore.instance.runTransaction((Transaction transaction) async {
-  //     await reference.document(id).updateData({
-  //       "name": newName,
-  //     }).catchError((error) {
-  //       print(error);
-  //     });
-  //   });
-  // }
 }
